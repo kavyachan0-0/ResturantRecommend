026eece262c384724e532c815e0aa257d411e556
kavyachan0-0 <kavyachan5@gmail.com>
2023-11-01 20:26:55 +0530

    made tempate using ejs and downloded ejs extension

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 const fs = require('fs');
 const path = require('path');
+
 const express = require('express');
 
 const app = express();
 
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:false}));
 
+app.get('/',function(req,res){
+   res.render('index');
+})
 app.get('/restaurants',function(req,res){
-  const htmlFilePath  = path.join(__dirname,'views','restaurants.html');
-   res.sendFile(htmlFilePath);
+   const filePath = path.join(__dirname,'data','restaurant.json');
+
+   const fileData = fs.readFileSync(filePath);
+   const storedRestaurants = JSON.parse(fileData);
+
+   res.render('restaurants',{ numberOfRestaurants: storedRestaurants.length,
+      resturants:storedRestaurants });
 });
 app.get('/recommend',function(req,res){
-   const htmlFilePath  = path.join(__dirname,'views','recommend.html');
-   res.sendFile(htmlFilePath);
+   res.render('recommend');
 }); 
-app.get('/',function(req,res){
-   const htmlFilePath  = path.join(__dirname,'views','index.html');
-   res.sendFile(htmlFilePath);
-});
-app.get('/confirm',function(req,res){
-const htmlFilePath  = path.join(__dirname,'views','confirm.html');
-   res.sendFile(htmlFilePath);
-});
-app.get('/about',function(req,res){
-   const htmlFilePath  = path.join(__dirname,'views','about.html');
-   res.sendFile(htmlFilePath);
-});
 
 app.post('/recommend',function(req,res){
    const restaurant = req.body;
@@ -40,6 +39,13 @@ app.post('/recommend',function(req,res){
 
    res.redirect('/confirm');
 });  
+
+app.get('/confirm',function(req,res){
+   res.render('confirm');
+});
+app.get('/about',function(req,res){
+   res.render('about');
+});
 
 app.listen(3001);
 
